cd3ca46b6736b79fbc3915b25cdc6776477dc81f
dennysora <dennysora.main@gmail.com>
2025-08-19 11:43:53 +0800

    feat: Update agui_to_sse to use UUID for event ID and add timestamp

--- src/adk_agui_middleware/tools/convert.py
+++ src/adk_agui_middleware/tools/convert.py
@@ -1,13 +1,15 @@
 import time
+import uuid
 
 from ag_ui.core import BaseEvent
 
 
 def agui_to_sse(event: BaseEvent) -> dict[str, str]:
+    event.timestamp = int(time.time() * 1000)
     return {
         "data": event.model_dump_json(
             by_alias=True, exclude_none=True, exclude={"type"}
         ),
         "event": event.type.value,
-        "id": str(int(time.time() * 1000)),
+        "id": str(uuid.uuid4()),
     }
