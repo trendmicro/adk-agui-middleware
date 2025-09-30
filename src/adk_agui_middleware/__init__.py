792f549543c33b7286774ebcc61071e90495a696
dennysora <dennysora.main@gmail.com>
2025-09-30 17:26:41 +0800

    feat: Bump version to 1.3.0 for release

--- src/adk_agui_middleware/__init__.py
+++ src/adk_agui_middleware/__init__.py
@@ -16,7 +16,7 @@ from .service.sse_service import SSEService
 
 __author__ = "Denny Lee"
 __email__ = "denny_lee@trendmicro.com"
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 
 __all__ = [
     "SSEService",
